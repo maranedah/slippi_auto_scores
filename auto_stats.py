e9d66af42a93a739accf7bddfae38dd8fa857fbe
maranedah <mauricio.araneda@ug.uchile.cl>
2023-09-26 19:46:00 -0300

    precommit

--- auto_stats.py
+++ auto_stats.py
@@ -1,34 +1,33 @@
-import urllib.request
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-import time
 from selenium.webdriver.firefox.options import Options
 
 
-
-
-
-
 def getStatsUrl(slp_paths):
-	paths = '\n'.join(slp_paths)
-	stats_url = "https://vince.id.au/slippi-stats/"
+    paths = "\n".join(slp_paths)
+    stats_url = "https://vince.id.au/slippi-stats/"
 
-	options = Options()
-	options.add_argument('--headless')
-	browser = webdriver.Firefox(firefox_binary=FirefoxBinary(), options=options)
-	browser.get(stats_url)
-	browser.find_element_by_xpath(".//input").send_keys(paths)
-	button = WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.CLASS_NAME, "css-1fgnl9k")))
-	button.click()
-	
-	WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "css-uso7ts")))
-	url = browser.current_url
-	browser.quit()
-	print(url)
-	return url
+    options = Options()
+    options.add_argument("--headless")
+    browser = webdriver.Firefox(firefox_binary=FirefoxBinary(), options=options)
+    browser.get(stats_url)
+    browser.find_element_by_xpath(".//input").send_keys(paths)
+    button = WebDriverWait(browser, 30).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, "css-1fgnl9k"))
+    )
+    button.click()
 
-#slp_paths = ["C:\\Users\\mauri\\Documents\\Slippi\\vs YingLing\\Game_20210314T131439.slp", "C:\\Users\\mauri\\Documents\\Slippi\\vs YingLing\\Game_20210314T131759.slp", "C:\\Users\\mauri\\Documents\\Slippi\\vs YingLing\\Game_20210314T132157.slp"]
-#getStatsUrl(slp_paths)
+    WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "css-uso7ts"))
+    )
+    url = browser.current_url
+    browser.quit()
+    print(url)
+    return url
+
+
+# slp_paths = ["C:\\Users\\mauri\\Documents\\Slippi\\vs YingLing\\Game_20210314T131439.slp", "C:\\Users\\mauri\\Documents\\Slippi\\vs YingLing\\Game_20210314T131759.slp", "C:\\Users\\mauri\\Documents\\Slippi\\vs YingLing\\Game_20210314T132157.slp"]
+# getStatsUrl(slp_paths)
--- tournament.py
+++ tournament.py
@@ -1,26 +1,22 @@
 import json, glob, os, math, time
 from auto_score import updateScore, resetScore, updateChars
 from auto_stats import getStatsUrl
-from obs_script import changeStats, changeToScoreBoard, startRecording, stopRecording, changeToVenueCam
+from obs_script import changeStats, changeToScoreBoard, changeToVenueCam
 import vlc
 import os
-import pathlib
-from constants import (
-    ROOT_FOLDER,
-    SLIPPI_FOLDER,
-    scoreinfo_path,
-    spectate_path
-)
+from constants import scoreinfo_path, spectate_path
+
 player = vlc.MediaPlayer("melee_menu.flac")
-            
+
+
 def tourney_match():
 
     f = open(scoreinfo_path)
     scoreinfo = json.load(f)
     f.close()
 
-    bestOf = scoreinfo["bestOf"][-1] # "Bo3", "Bo5"
-    max_score = math.ceil(int(bestOf)/2)
+    bestOf = scoreinfo["bestOf"][-1]  # "Bo3", "Bo5"
+    max_score = math.ceil(int(bestOf) / 2)
     print("max_score", max_score)
     p1_score = 0
     p2_score = 0
@@ -30,7 +26,7 @@ def tourney_match():
     set_files = []
 
     while p1_score < max_score and p2_score < max_score:
-        
+
         new_n_files = len(os.listdir(spectate_path))
         print(new_n_files)
 
@@ -40,23 +36,22 @@ def tourney_match():
             print("Found new file")
             player.stop()
             changeToScoreBoard()
-            #startRecording()
+            # startRecording()
 
             # Change Characters in ScoreBoard
-            
-            updateChars("game.json")
-            
-            # GetLatestFile
-            list_of_files = glob.glob(f'{spectate_path}\\*.slp') 
-            latest_file = max(list_of_files, key=os.path.getctime)
 
+            updateChars("game.json")
+
+            # GetLatestFile
+            list_of_files = glob.glob(f"{spectate_path}\\*.slp")
+            latest_file = max(list_of_files, key=os.path.getctime)
 
             # Wait while latest file still changing size
             curr_size = os.path.getsize(latest_file)
             time.sleep(2)
             while curr_size != os.path.getsize(latest_file):
                 curr_size = os.path.getsize(latest_file)
-                time.sleep(2)   
+                time.sleep(2)
 
             # Añadir a lista de juegos del set
             set_files.append(latest_file)
@@ -65,13 +60,12 @@ def tourney_match():
             updateScore(latest_file)
             player.play()
             changeToVenueCam()
-            
-            
+
         # Updatear puntaje de los jugadores
         f = open(scoreinfo_path)
         scoreinfo = json.load(f)
         p1_score = scoreinfo["p1Score"]
-        p2_score = scoreinfo["p2Score"] 
+        p2_score = scoreinfo["p2Score"]
         print("puntajes:", p1_score, p2_score)
         f.close()
         time.sleep(0.5)
@@ -85,13 +79,13 @@ def tourney_match():
     # Cambiar estadisticas en el obs
     changeStats(url)
     resetScore()
-    #stopRecording()
+    # stopRecording()
     changeToVenueCam()
 
 
 if __name__ == "__main__":
     while True:
         try:
-            tourney_match() 
+            tourney_match()
         except KeyboardInterrupt:
             break
